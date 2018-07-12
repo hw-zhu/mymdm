47117453a2b15f8e16c821ebfc64a7d017255698
hwzhu <hwzhu@TAL-HZHU.talend.com>
2018-07-12 15:38:09 +0800

    TMDM-12390 NPE when deploy data model

---
+++ myWorld/src/org/talend/action/MyStartingClass.java
@@ -0,0 +1,11 @@
+package org.talend.action;
+
+public class MyStartingClass {
+
+	public static void main(String[] args) {
+		System.out.println("1111");
+		System.out.println("2222");
+		System.out.println("3333");
+	}
+
+}
