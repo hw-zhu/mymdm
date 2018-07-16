a8c03c5dd6fd29a9d84b013f88ae32488d116262
huawei.zhu <41101497+hw-zhu@users.noreply.github.com>
2018-07-16 14:40:08 +0800

    Bugfix/7.0/tmdm 123 (#6)

--- myWorld/src/org/talend/action/MyStartingClass.java
+++ myWorld/src/org/talend/action/MyStartingClass.java
@@ -5,6 +5,8 @@ public class MyStartingClass {
 	public static void main(String[] args) {
 		System.out.println("the latest version 7.0 for master");
 		System.out.println("the latest version 7.0 for master1");
+		System.out.println("the latest version 7.0 for fixBug 1");
+		System.out.println("the latest version 7.0 for fixBug 2");
 	}
 
 }
