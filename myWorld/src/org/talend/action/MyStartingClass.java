eba06f92437deda77ff2296c3a7e64f730435573
hw-zhu <hwzhu@talend.com>
2018-07-16 14:22:59 +0800

    TMDM-123 fix bug 2

--- myWorld/src/org/talend/action/MyStartingClass.java
+++ myWorld/src/org/talend/action/MyStartingClass.java
@@ -5,6 +5,7 @@ public class MyStartingClass {
 	public static void main(String[] args) {
 		System.out.println("the latest version 7.0 for master");
 		System.out.println("the latest version 7.0 for fixBug 1");
+		System.out.println("the latest version 7.0 for fixBug 2");
 	}
 
 }
