20ebffec6fe148cc8d182385b618f8b3a89114d0
huawei.zhu <41101497+hw-zhu@users.noreply.github.com>
2018-07-16 14:09:32 +0800

    TMD-123 test rebase (#4)

--- myWorld/src/org/talend/action/MyStartingClass.java
+++ myWorld/src/org/talend/action/MyStartingClass.java
@@ -3,7 +3,7 @@ package org.talend.action;
 public class MyStartingClass {
 
 	public static void main(String[] args) {
-		System.out.println("the latest version 7.1");
+		System.out.println("the latest version 7.0");
 
 	}
 
