eeea2c6af13abf4f07eaec512e80e5f9ae74707e
hw-zhu <hwzhu@talend.com>
2018-10-25 10:44:47 +0800

    aaaa

--- myWorld/src/org/talend/action/MyStartingClass.java
+++ myWorld/src/org/talend/action/MyStartingClass.java
@@ -3,12 +3,8 @@ package org.talend.action;
 public class MyStartingClass {
 
 	public static void main(String[] args) {
-		System.out.println("the latest version 7.0 for master2");
-		System.out.println("the latest version 7.0 for master12");
-		System.out.println("the latest version 7.0 for master3");
-		System.out.println("the latest version 7.0 for fixBug 1");
-		System.out.println("the latest version 7.0 for fixBug 2");
-		System.out.println("the latest version 7.0 for fixBug 3");
+		System.out.println("111");
+
 	}
 
 }
