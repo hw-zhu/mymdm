7c353b5eda842adf498f30e8a3ada88506c30237
huawei.zhu <hwzhu@talend.com>
2021-09-03 22:17:19 +0800

    test

--- myWorld/src/org/talend/action/InitTest.java
+++ myWorld/src/org/talend/action/InitTest.java
@@ -4,8 +4,7 @@ public class InitTest {
 
 	public static void main(String[] args) {
 		System.out.println("1");
-		System.out.println("2L");
-System.out.println("1r");
+		System.out.println("22L");
 	}
 
 }
